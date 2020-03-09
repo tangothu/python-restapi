d6bdd1cf0b361dc7cb7807f1a91322bbd30a02ee
Ge Tan <tange116@gmail.com>
2020-03-09 00:00:49 -0400

    add README

---
+++ python-restapi/README.md
@@ -0,0 +1,25 @@
+tasks add "write some code" 21/08/2019
+tasks list
+tasks list --expiring-today
+tasks done 3
+
+
+Notes:
+swagger UI:
+http://localhost:5000/api/ui/#/
+
+curl Examples: 
+GET(get 1):
+curl -X GET --header 'Accept: application/json' 'http://localhost:5000/api/tasks/1'
+
+GET(get all):
+curl -X GET --header 'Accept: application/json' 'http://localhost:5000/api/tasks'
+
+PUT(update):
+curl -X PUT --header 'Content-Type: application/json' --header 'Accept: application/problem+json' -d '{ "title": "my new task" }' 'http://localhost:5000/api/tasks/1'
+ 
+DELETE(delete):
+curl -X DELETE --header 'Accept: text/html' 'http://localhost:5000/api/tasks/3'
+
+POST(create):
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ "title": "Do 10000 JIRAs" }' 'http://localhost:5000/api/tasks'
--- python-restapi/tasks.sh
+++ python-restapi/tasks.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 # file: tasks.sh
 
+
 tasks() {
     if [[ $1 == "list" ]]; then
+        echo "Getting all tasks" >> /tmp/python_restapi_logfile.log
         command curl -X GET --header "Accept: application/json" "http://localhost:5000/api/tasks"
     elif [[ $1 == "done" ]]; then
         task_id=$2
-        echo "Done with task id ${task_id}"
+        echo "Done with task id ${task_id}" >> /tmp/python_restapi_logfile.log
         command curl -X DELETE --header "Accept: text/html" "http://localhost:5000/api/tasks/${task_id}"
     elif [[ $1 == "add" ]]; then
         task_title=$2
         task_time=$3
-        echo "Adding new task ${task_title}"
+        echo "Adding new task ${task_title} with due date ${task_time}" >> /tmp/python_restapi_logfile.log
         printf -v payload '{"title": "%s", "duedate": "%s"}' "${task_title}" "${task_time}"
         echo "$payload"
         echo "${payload}"
